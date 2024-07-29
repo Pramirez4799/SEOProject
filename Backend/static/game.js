64c6d45804e9fdadfb647b24b62de7cacf1dd8c5
Pramirez4799 <pram1347@iastate.edu>
2024-07-29 12:18:49 -0500

    fixed song not reloading properly but code still has bug with not updating

--- Backend/static/game.js
+++ Backend/static/game.js
@@ -5,81 +5,34 @@ const choicesArray = [];
 const songsArray = [];
 // Get playlistId from URL parameters and fetch songs
 const queryParams = getQueryParams();
-fetchSongs(queryParams.playlistId);
+initializePage();
 
-
-
-//functions below
-function fetchSongs(playlistId) {
-    fetch(`/playlist/${playlistId}/tracks`)
-        .then(response => response.json())
-        .then(data => {
-            if (data.error) {
-                console.error('Error fetching playlist songs:', data.error);
-                return;
-            }
-            // Log songs to the console
-            console.log('Songs:', data);
-            //add songs to songsArray with name and preview URLs
-            data.items.forEach(item => {
-                songsArray.push({ name: item.track.name, preview_url: item.track.preview_url });
-            });
-            console.log('array:', songsArray);
-            //select a random number and use that to determine song from array 
-            const randomIndex = Math.floor(Math.random() * songsArray.length);
-            correctSong = songsArray[randomIndex];
-            // First index is always the correct song
-            choicesArray[0] = correctSong;
-            console.log('Selected song:', correctSong);
-
-            // Remove the correct song from the array so that the other three options are different songs 
-            songsArray.splice(randomIndex, 1);
-            console.log('Updated array:', songsArray);
-
-            // Select three more different songs and add them to choicesArray
-            for (let i = 1; i < 4; i++) {
-                const anotherRandomIndex = Math.floor(Math.random() * songsArray.length);
-                const otherSong = songsArray[anotherRandomIndex];
-                choicesArray[i] = otherSong;
-                // Remove song from possible choices
-                songsArray.splice(anotherRandomIndex, 1);
-            }
-
-            // Shuffle choicesArray to randomize the position of the correct song
-            shuffleArray(choicesArray);
-
-            // Display songs in HTML elements
-            const songChoice1 = document.getElementById("song1");
-            const songChoice2 = document.getElementById("song2");
-            const songChoice3 = document.getElementById("song3");
-            const songChoice4 = document.getElementById("song4");
-            songChoice1.innerHTML = choicesArray[0].name;
-            songChoice2.innerHTML = choicesArray[1].name;
-            songChoice3.innerHTML = choicesArray[2].name;
-            songChoice4.innerHTML = choicesArray[3].name;
-
-            // Automatically play the correct song
-            playSong(correctSong.preview_url);
-
-            // Set up event listeners for the choices
-            songChoice1.addEventListener("click", function() {
-                handleGuess(choicesArray[0]);
-            });
-            songChoice2.addEventListener("click", function() {
-                handleGuess(choicesArray[1]);
-            });
-            songChoice3.addEventListener("click", function() {
-                handleGuess(choicesArray[2]);
-            });
-            songChoice4.addEventListener("click", function() {
-                handleGuess(choicesArray[3]);
-            });
-        })
-        .catch(error => {
-            console.error('Error fetching playlist songs:', error);
-        });
+//functions below!!!!!!!!!!!!!!!!!!!!!!!!
+async function initializePage() {
+    await fetchSongs(queryParams.playlistId);
+    loadSongsToPage();
 }
+//grab all songs and load to songs array (only need to do once )
+async function fetchSongs(playlistId) {
+    try {
+        const response = await fetch(`/playlist/${playlistId}/tracks`);
+        const data = await response.json();
 
+        if (data.error) {
+            console.error('Error fetching playlist songs:', data.error);
+            return;
+        }
+
+        //add songs to songsArray with name and preview URLs
+        data.items.forEach(item => {
+            songsArray.push({ name: item.track.name, preview_url: item.track.preview_url });
+        });
+
+        console.log('Songs:', songsArray);
+    } catch (error) {
+        console.error('Error fetching playlist songs:', error);
+    }
+}
 // function to handle the guess
 function handleGuess(selectedSong) {
     if (selectedSong.name === correctSong.name) {
@@ -88,7 +41,8 @@ function handleGuess(selectedSong) {
     } else {
         console.log("Wrongggggggggggggggg");
     }
-    fetchSongs(queryParams.playlistId);
+    //load songs again after a guess
+    loadSongsToPage();
 }
 
 // function to shuffle the choicesArray
@@ -111,9 +65,57 @@ function getQueryParams() {
     const params = new URLSearchParams(window.location.search);
     return Object.fromEntries(params.entries());
 }
-// function loadNextSong(){
 
-// }
 function loadSongsToPage(){
-    
+    //copy array of songs 
+    const copiedSongsArray = [...songsArray];
+    // Log songs to the console
+    console.log('Songs:', songsArray);
+    //select a random number and use that to determine song from array 
+    const randomIndex = Math.floor(Math.random() * songsArray.length);
+    correctSong = songsArray[randomIndex];
+    // First index is always the correct song
+    choicesArray[0] = correctSong;
+
+    // Remove the correct song from the array so that the other three options are different songs 
+    songsArray.splice(randomIndex, 1);
+
+    // Select three more different songs and add them to choicesArray
+    for (let i = 1; i < 4; i++) {
+        const anotherRandomIndex = Math.floor(Math.random() * songsArray.length);
+        const otherSong = copiedSongsArray[anotherRandomIndex];
+        choicesArray[i] = otherSong;
+        // Remove song from possible choices
+        copiedSongsArray.splice(anotherRandomIndex, 1);
+    }
+
+    // Shuffle choicesArray to randomize the position of the correct song
+    shuffleArray(choicesArray);
+
+    // Display songs in HTML elements
+    const songChoice1 = document.getElementById("song1");
+    const songChoice2 = document.getElementById("song2");
+    const songChoice3 = document.getElementById("song3");
+    const songChoice4 = document.getElementById("song4");
+    songChoice1.innerHTML = choicesArray[0].name;
+    songChoice2.innerHTML = choicesArray[1].name;
+    songChoice3.innerHTML = choicesArray[2].name;
+    songChoice4.innerHTML = choicesArray[3].name;
+
+    // Automatically play the correct song
+    playSong(correctSong.preview_url);
+
+    // Set up event listeners for the choices
+    songChoice1.addEventListener("click", function() {
+        handleGuess(choicesArray[0]);
+    });
+    songChoice2.addEventListener("click", function() {
+        handleGuess(choicesArray[1]);
+    });
+    songChoice3.addEventListener("click", function() {
+        handleGuess(choicesArray[2]);
+    });
+    songChoice4.addEventListener("click", function() {
+        handleGuess(choicesArray[3]);
+    });
 }
